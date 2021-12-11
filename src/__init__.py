83f25a4176431176a463ec3e1ac6eb9947b92cfd
vantruong2220 <1951052220truong@gmail.com>
2021-12-11 22:41:47 +0700

    aaaa

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:12345678@localhost/hotel?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
-
-
 db = SQLAlchemy(app=app)
+
+
