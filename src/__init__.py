d8acf185dd7f755c1b68752b4005dd3478aea12b
hieudnguyen <hieudnguyen@meu-solutions.com>
2021-12-13 21:30:25 +0700

    WIP stats.html

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Duchieu200301@localhost/hotel_management_db?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,17 @@
-from sqlalchemy import text, func
+from flask import request
+from sqlalchemy import text, func, extract
 
 from src import app, db
 from src.models import Room, TypeRoom, ReceiptDetail, User, Receipt, ChangePolicyNumber, RentalVoucher, RentalVoucherDetail, TypeVisit
 from sqlalchemy.orm import Session
+from datetime import datetime
 
 
 def get_all_type_rooms():
     return TypeRoom.query.all()
 
 
+# Get used quantity type room in current month
 def get_used_quantity_type_room_in_month():
-    query = db.session.query(func.count(TypeRoom.id), TypeRoom.type_room_name).filter(Room.type_room_id == TypeRoom.id).filter(Room.id == ReceiptDetail.room_id).group_by(TypeRoom.type_room_name)
+    query = db.session.query(TypeRoom.id, TypeRoom.type_room_name, func.count(TypeRoom.id), ).filter(Room.type_room_id == TypeRoom.id).filter(Room.id == ReceiptDetail.room_id).filter(extract('month', ReceiptDetail.rental_date) == datetime.now().month).group_by(TypeRoom.id, TypeRoom.type_room_name)
     return query.all()
