1c3fa211a01920c4f12505f8ffb4a07cb58376d5
Your Name <1951052155phuc@ou.edu.vn>
2021-12-19 10:31:51 +0700

    trang dang nhap va dang ky

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ from src import app, db
 from src.models import Room, TypeRoom, ReceiptDetail, User, Receipt, ChangePolicyNumber, RentalVoucher, RentalVoucherDetail, TypeVisit
 from sqlalchemy.orm import Session
 from datetime import datetime
+import hashlib
 
 
 def get_all_type_rooms():
@@ -15,3 +16,26 @@ def get_all_type_rooms():
 def get_used_quantity_type_room_in_month():
     query = db.session.query(TypeRoom.id, TypeRoom.type_room_name, func.count(TypeRoom.id), ).filter(Room.type_room_id == TypeRoom.id).filter(Room.id == ReceiptDetail.room_id).filter(extract('month', ReceiptDetail.rental_date) == datetime.now().month).group_by(TypeRoom.id, TypeRoom.type_room_name)
     return query.all()
+
+
+def add_user(username, password, email):
+    password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+    user = User(username=username.strip(), password=password,
+                email=email)
+
+    db.session.add(user)
+    db.session.commit()
+
+
+
+def check_login(username, password):
+    if username and password:
+        password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest()) #hash bằng md5 khi create password thì cũng hash bằng md5 khi login
+
+        return User.query.filter(User.username.__eq__(username.strip()),
+                                User.password.__eq__(password)).first()
+
+
+
+def get_user_by_id(user_id):
+    return User.query.get(user_id)
