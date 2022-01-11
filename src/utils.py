ef909064be5806bf196cbe8a37ee852033bae772
hieudnguyen <hieudnguyen@meu-solutions.com>
2022-01-11 08:54:42 +0700

    run

--- src/utils.py
+++ src/utils.py
@@ -14,8 +14,8 @@ from src.models import Room, TypeRoom, ReceiptDetail, User, Receipt, RentalVouch
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="12345678",
-    database="hotel"
+    password="Duchieu200301",
+    database="hotel_management_db"
 )
 
 
